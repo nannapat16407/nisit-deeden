c08609a0d96bf16295caa4963e98bc3ebc71535a
punsnx <79169700+punsnx@users.noreply.github.com>
2026-01-29 00:47:16 +0700

    update LeftMenu

--- app/faculty/page.tsx
+++ app/faculty/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import LeftMenu from "@/components/ui/LeftMenu";
 
-function Home() {
+function Dashboard() {
   const buttons = [
     {
       name: "หน้าหลัก",
@@ -25,12 +25,12 @@ function Home() {
       <LeftMenu buttons={buttons} />
       <div className="w-full p-4 text-black">
         <div className="w-full h-24 bg-emerald-600 rounded-2xl shadow-md flex items-center">
-          <div className="pl-8 text-white font-bold text-xl">HOME</div>
+          <div className="pl-8 text-white font-bold text-xl">DASHBOARD</div>
         </div>
-        <div className="pl-8 pt-4">this is home page</div>
+        <div className="pl-8 pt-4">this is dashboard</div>
       </div>
     </div>
   );
 }
 
-export default Home;
+export default Dashboard;
