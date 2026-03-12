45202c79222fc5c3a71b56fef2914cfa47d757b3
pleng <pacharaprompang1@gmail.com>
2026-03-12 15:08:47 +0700

    add build docker in ci/cd github workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone", // Required for Docker production build
+  output: "standalone", 
 };
 
 export default nextConfig;
